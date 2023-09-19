eb72390aab565ab731bfc5a66d6b30d28b04492f
User <girain3181@gmail.com>
2023-09-19 11:17:06 +0300

    finish

--- src/redux/slices/loadingAndErrorSlice.ts
+++ src/redux/slices/loadingAndErrorSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, isFulfilled, isRejected } from '@reduxjs/toolkit';
+import { createSlice, isFulfilled, isPending, isRejected } from '@reduxjs/toolkit';
 
 interface IState {
     error:string
@@ -16,20 +16,32 @@ const loadingAndErrorSlice = createSlice({
     reducers: {},
     extraReducers: builder =>
         builder
-            .addMatcher(
-                (action) => {
-                    if (action.type.startsWith('deleteOrAdd/favoriteSlice')) {
-                        return !action.type.startsWith('deleteOrAdd/favoriteSlice');
-                    }else if (action.type.startsWith('searchMovies/searchSlice')) {
-                        return !action.type.startsWith('searchMovies/searchSlice');
-                    }
-                },
+        // .addMatcher(
+        //     (action) => {
+        //         if (action.type.startsWith('deleteOrAdd/favoriteSlice')) {
+        //             return !action.type.startsWith('deleteOrAdd/favoriteSlice');
+        //         }else if (action.type.startsWith('searchMovies/searchSlice')) {
+        //             return !action.type.startsWith('searchMovies/searchSlice');
+        //         }
+        //     },
+        //
+        //     (state) => {
+        //         state.isLoading = true;
+        //         state.error = null;
+        //     },
+        // )
 
-                (state) => {
-                    state.isLoading = true;
-                    state.error = null;
-                },
-            )
+        // 19.09.23 периписав addMatcher() на addMatcher(isPending())
+
+            .addMatcher(isPending(), (state, action) => {
+                const isAddFavorite = action.type.includes('deleteOrAdd');
+                const isSearchMovies = action.type.includes('searchMovies');
+
+                if ( !isAddFavorite && !isSearchMovies ) state.isLoading = true;
+
+                state.error = null;
+            })
+
             .addMatcher(isRejected(), (state, action) => {
                 state.error = action.payload as string;
                 state.isLoading = false;
@@ -43,3 +55,4 @@ const loadingAndErrorSlice = createSlice({
 const { reducer: loadingAndErrorReducer } = loadingAndErrorSlice;
 
 export { loadingAndErrorReducer };
+
